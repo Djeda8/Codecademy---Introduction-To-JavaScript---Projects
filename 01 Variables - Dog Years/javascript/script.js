580a97d09c9825220dda22986dcf807f6f494702
Djeda8 <43439678+Djeda8@users.noreply.github.com>
2019-05-01 11:06:25 +0200

    add variables

---
+++ 01 Variables - Dog Years/javascript/script.js
@@ -0,0 +1,12 @@
+const myAge=42;
+
+let earlyYears= 2;
+earlyYears *= 10.5;
+
+let laterYears = myAge - 2;
+laterYears *= 4;
+
+let myAgeInDogYears = earlyYears + laterYears;
+let myName = "Daniel".toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} old in human years which is ${myAgeInDogYears} old in dog years`);
